39bc59d212f609ee082fa99f63ce3e8a0ce24f9c
Michael Joyner <joyner.michael@gmail.com>
2019-05-03 11:13:57 +0800

    use min height for shoutout banner

--- resources/views/front/home/villa-shoutout.blade.php
+++ resources/views/front/home/villa-shoutout.blade.php
@@ -1,4 +1,4 @@
-<section class="villa-shoutout banner-height flex justify-center items-center">
+<section class="villa-shoutout min-section-height flex justify-center items-center">
     <div class="max-w-sm p-8 bg-opaque flex flex-col items-center mx-4 md:mx-0">
         @include('svgs.logos.villa_logo_cuisine')
         <p class="mb-8  font-serif text-black text-center">{{ trans('homepage.shoutout.content') }}</p>
